d8ccdf0d6cb3f8497702cb73cdb518be46fb0611
Karen Brakband <karen@impres.nl>
2019-06-19 20:23:15 +0200

    A bit of styling

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ function js(){
 }
 
 const clean_css = require('gulp-clean-css');
-//const auto_prefixer = require('gulp-autoprefixer');
+const auto_prefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 
@@ -34,7 +34,7 @@ function scss(){
     return gulp.src(scssFiles)
         .pipe(sass().on('error', sass.logError))
         .pipe(clean_css({compatibility: 'ie9'}))
-        //.pipe(auto_prefixer('last 2 version', 'safari 5', 'ie 9'))
+        .pipe(auto_prefixer('last 2 version', 'safari 5', 'ie 9'))
         .pipe(concat('style.min.css'))
         .pipe(gulp.dest('./dist'));
 }
