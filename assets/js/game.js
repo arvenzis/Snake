4d618a36e42e0aef8a7a696636ca1ecca574b72e
Karen Brakband <karen@impres.nl>
2019-06-20 21:10:27 +0200

    Show a grid on the field based on the created array

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@ let game = (function() {
     let verticalSize = 400;
     let gridSize = 20;
 
-    let buildGameField = (function() {
+    let buildGameFieldArray = (function() {
         let verticalIterations = verticalSize / gridSize;
         let horizontalIterations = horizontalSize / gridSize;
 
@@ -13,13 +13,42 @@ let game = (function() {
                 gameFieldArray[i] = [];
             }
             for (let j = 0; j < verticalIterations ; j++) {
-                gameFieldArray[i][j] = null;
+                if (i === 10 && j === 8) {
+                    gameFieldArray[i][j] = 'snake';
+                } else {
+                    gameFieldArray[i][j] = null;
+                }
             }
         }
-        console.log(gameFieldArray);
+
+        drawGameField(gameFieldArray);
+    });
+
+    let drawGameField = (function(gameFieldArray) {
+        for (let i = 0; i < gameFieldArray.length ; i++) {
+            for (let j = 0; j < gameFieldArray[1].length ; j++) {
+                if (gameFieldArray[i][j] === null) {
+                    let emptyTile = createGameElement('game__tile', i.toString(), j.toString());
+                    $(emptyTile).appendTo('.game__container');
+                }
+                if (gameFieldArray[i][j] === 'snake') {
+                    let snake = createGameElement('snake', i.toString(), j.toString());
+                    $(snake).appendTo('.game__container');
+                }
+            }
+        }
+    });
+
+    let createGameElement = (function(classValue, dataH, dataV) {
+        let element = document.createElement('div');
+        element.setAttribute('class', classValue);
+        element.setAttribute('data-h', dataH);
+        element.setAttribute('data-v', dataV);
+
+        return element;
     });
 
     return {
-        buildGameField: buildGameField
+        buildGameFieldArray: buildGameFieldArray
     }
 })();
