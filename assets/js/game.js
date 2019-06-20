57434e9e49e99d1ea9ea1d7f860869d6e6173989
Karen Brakband <karen@impres.nl>
2019-06-20 22:31:18 +0200

    Gallant code.

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,16 +7,23 @@ let game = (function() {
         let verticalIterations = verticalSize / gridSize;
         let horizontalIterations = horizontalSize / gridSize;
 
+        let targetH = decideTargetLocation(30);
+        let targetV = decideTargetLocation(20);
+
         let gameFieldsArray = [];
         for (let i = 0; i < horizontalIterations ; i++) {
             if(!gameFieldsArray[i]) {
                 gameFieldsArray[i] = [];
             }
             for (let j = 0; j < verticalIterations ; j++) {
-                if (i === 10 && j === 8) {
+                if (i === 15 && j === 15) {
                     gameFieldsArray[i][j] = 'snake';
                 } else {
                     gameFieldsArray[i][j] = null;
+                }
+
+                if (i === targetH && j === targetV) {
+                    gameFieldsArray[i][j] = 'target';
                 }
             }
         }
@@ -27,13 +34,19 @@ let game = (function() {
     let drawGameBoard = (function(gameFieldsArray) {
         for (let i = 0; i < gameFieldsArray.length ; i++) {
             for (let j = 0; j < gameFieldsArray[1].length ; j++) {
-                if (gameFieldsArray[i][j] === null) {
-                    let emptyTile = createGameElement('game__tile', i.toString(), j.toString());
-                    $(emptyTile).appendTo('.game__container');
-                }
-                if (gameFieldsArray[i][j] === 'snake') {
-                    let snake = createGameElement('snake', i.toString(), j.toString());
-                    $(snake).appendTo('.game__container');
+                switch(gameFieldsArray[i][j]) {
+                    case null:
+                        let emptyTile = createGameElement('game__tile', i.toString(), j.toString());
+                        $(emptyTile).appendTo('.game__container');
+                        break;
+                    case 'snake':
+                        let snake = createGameElement('snake', i.toString(), j.toString());
+                        $(snake).appendTo('.game__container');
+                        break;
+                    case 'target':
+                        let target = createGameElement('target', i.toString(), j.toString());
+                        $(target).appendTo('.game__container');
+                        break;
                 }
             }
         }
@@ -46,6 +59,11 @@ let game = (function() {
         element.setAttribute('data-v', dataV);
 
         return element;
+    });
+
+    let decideTargetLocation = (function(max) {
+        let num = Math.floor(Math.random() * max) + 1;
+        return (num === 15) ? decideTargetLocation(max) : num;
     });
 
     return {
